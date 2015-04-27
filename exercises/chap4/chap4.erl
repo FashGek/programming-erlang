d28603872fd1271b91aa84b66133590f57573821
JoeBew42 <joebew42@gmail.com>
2015-04-27 17:33:22 +0200

    Adding my_time_func implementation

--- exercises/chap4/chap4.erl
+++ exercises/chap4/chap4.erl
@@ -1,6 +1,6 @@
 -module(chap4).
 
--export([my_tuple_to_list/1]).
+-export([my_tuple_to_list/1, my_time_func/1]).
 
 my_tuple_to_list(T) ->
     my_tuple_to_list(size(T), T, []).
@@ -11,4 +11,8 @@ my_tuple_to_list(Size, T, Acc) ->
     Element = element(Size, T),
     my_tuple_to_list(Size - 1, T, [Element|Acc]).
 
-
+my_time_func(F) ->
+    {_, StartSec, StartMicroSec} = now(),
+    F(),
+    {_, EndSec, EndMicroSec} = now(),
+    {EndSec - StartSec, abs(EndMicroSec - StartMicroSec)}.
