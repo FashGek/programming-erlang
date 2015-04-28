92e8c7e0de356aaede1798386a232199a9b8ee76
JoeBew42 <joebew42@gmail.com>
2015-04-28 20:29:14 +0200

    Add my_date_string

--- exercises/chap4/lib_misc.erl
+++ exercises/chap4/lib_misc.erl
@@ -1,6 +1,6 @@
 -module(lib_misc).
 
--export([my_tuple_to_list/1, my_time_func/1]).
+-export([my_tuple_to_list/1, my_time_func/1, my_date_string/0]).
 
 my_tuple_to_list(T) ->
     my_tuple_to_list(size(T), T, []).
@@ -17,3 +17,10 @@ my_time_func(F) ->
     End = now(),
     timer:now_diff(End, Start).
 
+my_date_string() ->
+    {Year, Month, Day} = date(),
+    {Hour, Minute, Second} = time(),
+    io:format("~s the ~p, ~p. ~p:~p:~p~n", [month_to_string(Month), Day, Year, Hour, Minute, Second]).
+
+month_to_string(Month) ->
+    element(Month, {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'}).
