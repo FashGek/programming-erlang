1c77f536709e74df5eb698705cb91b5dd6bc96ad
JoeBew42 <joebew42@gmail.com>
2015-04-29 16:47:46 +0200

    Add split and split2 functions

--- exercises/chap4/math_functions.erl
+++ exercises/chap4/math_functions.erl
@@ -1,6 +1,6 @@
 -module(math_functions).
 
--export([even/1, odd/1, filter/2]).
+-export([even/1, odd/1, filter/2, split/1, split2/1]).
 
 even(Number) ->
     Number rem 2 == 0.
@@ -10,3 +10,20 @@ odd(Number) ->
 
 filter(F, List) ->
     [Element || Element <- List, F(Element)].
+
+split(List) ->
+    Even = filter(fun(X) -> even(X) end, List),
+    Odd = filter(fun(X) -> odd(X) end, List),
+    {Even, Odd}.
+
+split2(List) ->
+    split(List, [], []).
+
+split([], Even, Odd) ->
+    {lists:reverse(Even), lists:reverse(Odd)};
+
+split([H|T], Even, Odd) ->
+    case even(H) of
+        true -> split(T, [H|Even], Odd);
+        false -> split(T, Even, [H|Odd])
+    end.
