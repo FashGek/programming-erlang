9edbc6b193de93139426a47cfa4296b10ab5f5c0
JoeBew42 <joebew42@gmail.com>
2015-04-29 16:23:52 +0200

    Add filter function

--- exercises/chap4/math_functions.erl
+++ exercises/chap4/math_functions.erl
@@ -1,9 +1,12 @@
 -module(math_functions).
 
--export([even/1, odd/1]).
+-export([even/1, odd/1, filter/2]).
 
 even(Number) ->
     Number rem 2 == 0.
 
 odd(Number) ->
     not even(Number).
+
+filter(F, List) ->
+    [Element || Element <- List, F(Element)].
