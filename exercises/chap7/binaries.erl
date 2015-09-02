066c96c05f2977183eeb3111bb2ec6eac1fbb131
JoeBew42 <joebew42@gmail.com>
2015-09-02 09:29:04 +0200

    First attempt for term_to_packet

--- exercises/chap7/binaries.erl
+++ exercises/chap7/binaries.erl
@@ -1,6 +1,8 @@
 -module(binaries).
 
--export([reverse/1, reverse2/1]).
+-export([reverse/1, reverse2/1, term_to_packet/1]).
+
+% First exercise
 
 reverse(Bin) ->
     reverse_helper(Bin, byte_size(Bin), <<>>).
@@ -19,3 +21,10 @@ reverse2(<<>>, Acc) ->
     Acc;
 reverse2(<<Head:1/binary, Rest/binary>>, Acc) ->
     reverse2(Rest, <<Head/binary, Acc/binary>>).
+
+% Second exercise
+
+term_to_packet(Term) ->
+    Binary = term_to_binary(Term),
+    Length = byte_size(Binary),
+    <<Length:32/integer, Binary:Length/binary>>.
