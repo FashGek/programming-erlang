3c33fa19175dd918288cbfc66b34aaabbe6c1f90
JoeBew42 <joebew42@gmail.com>
2015-09-02 08:57:55 +0200

    second version of reverse binary using pattern matching

--- exercises/chap7/binaries.erl
+++ exercises/chap7/binaries.erl
@@ -1,6 +1,6 @@
 -module(binaries).
 
--export([reverse/1]).
+-export([reverse/1, reverse2/1]).
 
 reverse(Bin) ->
     reverse_helper(Bin, byte_size(Bin), <<>>).
@@ -10,3 +10,12 @@ reverse_helper(_, 0, Reversed) ->
 reverse_helper(Bin, Length, Reversed) ->
     Byte = binary:at(Bin, Length - 1),
     reverse_helper(Bin, Length - 1, <<Reversed/binary, Byte>>).
+
+
+reverse2(Bin) ->
+    reverse2(Bin, <<>>).
+
+reverse2(<<>>, Acc) ->
+    Acc;
+reverse2(<<Head:1/binary, Rest/binary>>, Acc) ->
+    reverse2(Rest, <<Head/binary, Acc/binary>>).
