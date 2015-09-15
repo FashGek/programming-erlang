3545ed42b79a90064f11dbe387f7ccd6e88dcb46
JoeBew42 <joebew42@gmail.com>
2015-09-15 14:31:22 +0200

    Add test for term_to_packet and packet_to_term

--- exercises/chap7/binaries.erl
+++ exercises/chap7/binaries.erl
@@ -1,6 +1,7 @@
 -module(binaries).
 
 -export([reverse/1, reverse2/1, term_to_packet/1, packet_to_term/1]).
+-export([test_term_to_packet/0, test_packet_to_term/0]).
 
 % First exercise
 
@@ -33,3 +34,18 @@ term_to_packet(Term) ->
 
 packet_to_term(<<Length:32/integer, Binary:Length/binary>>) ->
     binary_to_term(Binary).
+
+% Fourth exercise
+
+test_term_to_packet() ->
+    Term = i_am_a_term,
+    Binary = term_to_binary(Term),
+    Length = byte_size(Binary),
+    <<Length:32/integer, Binary:Length/binary>> = term_to_packet(Term),
+    test_pass.
+
+test_packet_to_term() ->
+    Term = i_am_a_term,
+    Packet = term_to_packet(Term),
+    Term = packet_to_term(Packet),
+    test_pass.
