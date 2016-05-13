37246c68f4b3235070ba18ae56390ca3b5dac763
JoeBew42 <joebew42@gmail.com>
2016-05-13 14:12:05 +0200

    add keep_alive example

--- exercises/chap13/lib_misc.erl
+++ exercises/chap13/lib_misc.erl
@@ -4,6 +4,9 @@
 
 -export([echo/0]).
 
+-export([keep_alive/3]).
+-export([die_after/1]).
+
 on_exit(Pid, Fun) ->
     spawn(fun() ->
                   Ref = monitor(process, Pid),
@@ -17,4 +20,17 @@ echo() ->
     receive
         Message ->
             list_to_atom(Message)
+    end.
+
+keep_alive(Module, Function, Args) ->
+    Pid = spawn(Module, Function, Args),
+    on_exit(Pid, fun(_Why) -> keep_alive(Module, Function, Args) end).
+
+die_after(Timeout) ->
+    io:format("I'm up with PID: [~p]~n", [self()]),
+    receive
+    after
+        Timeout ->
+            io:format("I'm down with PID: [~p]~n", [self()]),
+            done
     end.
