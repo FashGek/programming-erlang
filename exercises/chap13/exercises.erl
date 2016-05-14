afd6753920512d5044b37ac2a34cd4952b25b541
JoeBew42 <joebew42@gmail.com>
2016-05-14 21:00:36 +0200

    add second exercise of the chapter 13

--- exercises/chap13/exercises.erl
+++ exercises/chap13/exercises.erl
@@ -3,7 +3,10 @@
 -export([my_spawn/3]).
 -export([my_spawn_monitor/3]).
 
+-export([on_exit/2]).
+
 -export([die_after/1]).
+
 
 %% Exercise 1
 
@@ -19,6 +22,20 @@ my_spawn_monitor(Mod, Func, Args) ->
             Duration = erlang:convert_time_unit(Stop - Start, native, milli_seconds),
             io:format("The Pid ~p has exited with [~p], total duration of: ~p ms !~n", [Pid, Why, Duration])
     end.
+
+%% Exercise 2
+
+on_exit(Pid, Fun) ->
+    spawn(fun() ->
+                  Ref = monitor(process, Pid),
+                  Start = erlang:monotonic_time(),
+                  receive
+                      {'DOWN', Ref, process, Pid, Why} ->
+                          Stop = erlang:monotonic_time(),
+                          Duration = erlang:convert_time_unit(Stop - Start, native, milli_seconds),
+                          Fun(Why, Duration)
+                  end
+          end).
 
 %% Utility code
 
